a6ef210e0f3887d5a5a2b5a1f086441afab72207
JonaCaucota <jonacau12@gmail.com>
2023-05-06 22:50:48 -0300

    Modified CarritoItem.class

--- SushiPopG5/Models/CarritoItem.cs
+++ SushiPopG5/Models/CarritoItem.cs
@@ -7,11 +7,15 @@ namespace SushiPopG5.Models
     {
         [Key]
         public int Id { get; set; }
+        
         [ForeignKey("CarritoId")]
         public Carrito? Carrito { get; set; }
+        
         [ForeignKey("ProductoId")]
         public Producto? Producto { get; set; }
+        
         public double Precio { get; set; }
+        
         public int Cantidad { get; set; }
     }
 }
