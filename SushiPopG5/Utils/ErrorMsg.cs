4cee177863c52ca5d178144876a7cd974c6e5001
felibuscaglia <felipebbuscaglia@gmail.com>
2023-06-04 14:23:13 -0300

    ErrorMinCaracteres ahora muestra los caracteres minimos

--- SushiPopG5/Utils/ErrorMsg.cs
+++ SushiPopG5/Utils/ErrorMsg.cs
@@ -4,6 +4,6 @@
     {
         public const string ErrorCampoRequerido = "Error campo requerido";
         public const string ErrorMaxCaracteres = "Error cantidad maxima de caracteres es {0}";
-        public const string ErrorMinCaracteres = "Error cantidad minima de caracteres es {0}";
+        public const string ErrorMinCaracteres = "Error cantidad minima de caracteres es {1}";
     }
 }
